f237eaad6a343af5aa08943a0e2c9bf51f0b255a
Jotrorox <Jotrorox@gmail.com>
2023-06-19 23:03:35 +0200

    reverting server support -> see issue 3

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,19 @@ mod simple_card;
 mod tag_updater;
 mod util;
 mod users;
-mod normal_mode;
-mod server_mode;
-mod mode_selector;
 
 use markup_updater::*;
 use tag_updater::*;
 use util::*;
-use normal_mode::*;
-use server_mode::*;
-use mode_selector::*;
 
 #[tokio::main]
 async fn main() {
-    if get_if_server_mode() {
-        server_mode().await;
-    } else {
-        normal_mode().await;
+    let users: Users = Users::new(vec![Key::new()]);
+
+    loop {
+        for key in &users.keys {
+            check_for_due_cards(&key).await;
+        }
+        thread::sleep(time::Duration::from_secs(60 * /* Time in minutes: */ 15));
     }
 }
