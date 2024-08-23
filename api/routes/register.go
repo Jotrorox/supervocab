f50d50cb6d3a7195e2ec817ef0d2c4f4b31cae3f
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-08-23 18:20:32 +0200

    Refactoring the route

---
+++ api/routes/register.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"database/sql"
+	"log"
+	"supervocab/db"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterUserHandler(database *sql.DB) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		token := c.Params("token")
+
+		if token == "" {
+			return c.Status(400).JSON(fiber.Map{
+				"status":  "fail",
+				"message": "Invalid token",
+			})
+		}
+
+		if err := db.InsertUser(database, token); err != nil {
+			log.Println(err)
+			return c.Status(500).JSON(fiber.Map{
+				"status":  "fail",
+				"message": "Internal Server Error",
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"status":  "success",
+			"message": "Registered successfully",
+		})
+	}
+}
