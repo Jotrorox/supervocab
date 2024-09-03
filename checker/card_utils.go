6f9f0c3de522f5b0ad00748ed28d607cba3f4ac4
Johanens (Jotrorox) Müller <mail@jotrorox.com>
2024-09-03 19:42:14 +0200

    Enhance user card fetching in checker module
    Refactor function name to follow camelCase convention. Add logic to retrieve all users from the database and fetch their cards periodically.

--- checker/card_utils.go
+++ checker/card_utils.go
@@ -8,7 +8,7 @@ import (
 	"supervocab/util"
 )
 
-func get_cards(token string) {
+func getCards(token string) {
 	url := "https://api.supernotes.app/v1/cards/get/select"
 
 	payload := strings.NewReader("{\n  \"include_membership_statuses\": [\n    2\n  ],\n  \"filter_group\": {\n    \"operator\": \"and\",\n    \"filters\": [\n      {\n        \"type\": \"tag\",\n        \"operator\": \"contains\",\n        \"arg\": \"supervocab\"\n      }\n    ]\n  },\n  \"sort_type\": 0,\n  \"sort_ascending\": true,\n  \"limit\": 0\n}")
--- checker/checker.go
+++ checker/checker.go
@@ -3,12 +3,23 @@ package checker
 import (
 	"database/sql"
 	"supervocab/config"
+	"supervocab/db"
+	"supervocab/util"
 	"time"
 )
 
 func StartChecker(database *sql.DB, cfg *config.Config) {
 	for {
 		checkTokens(database)
+
+		users, err := db.GetAllUsers(database)
+		if err != nil {
+			util.HandleFatalError(err, "Failed to get users")
+		}
+
+		for _, user := range users {
+			getCards(user.Token)
+		}
 
 		time.Sleep(1 * time.Minute)
 	}
